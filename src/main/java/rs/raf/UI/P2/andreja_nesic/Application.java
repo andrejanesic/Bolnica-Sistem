cc9beb042a3dd921a422040d73592b58fe472bd3
Andreja Nesic <anesic3119rn@raf.rs>
2022-01-08 20:28:39 +0100

    initial commit

---
+++ src/main/java/rs/raf/UI/P2/andreja_nesic/Application.java
@@ -0,0 +1,13 @@
+package rs.raf.UI.P2.andreja_nesic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/rs/raf/UI/P2/andreja_nesic/ApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.raf.UI.P2.andreja_nesic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
